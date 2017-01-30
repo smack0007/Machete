720608cebddc6e1af6c76061ef49ce3e6dc10578
smack0007 <zachary.snow@gmail.com>
2017-01-30 17:08:15 +0100

    NUnit is now in repo and not a NuGet package.

--- Source/Machete/StringBuilderExtensions.cs
+++ Source/Machete/StringBuilderExtensions.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Machete
 {
-	internal static class StringBuilderExtensions
+    internal static class StringBuilderExtensions
 	{
 		public static void AppendLine(this StringBuilder sb, string format, params object[] args)
 		{
